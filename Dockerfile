abfad01a17fa322764dce475c6ddd91165354c21
youneskebli <youneskebli9@gmail.com>
2024-01-17 09:03:03 +0100

    chore: modify the app.py

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
-# Use an official Python runtime as a parent image
 FROM python:3.8
 
-# Install system dependencies for WeasyPrint, OpenCV, and TensorFlow
 RUN apt-get update && apt-get install -y \
     build-essential \
     libffi-dev \
@@ -13,23 +11,15 @@ RUN apt-get update && apt-get install -y \
     shared-mime-info \
     libgl1-mesa-dev
 
-# Set the working directory in the container
 WORKDIR /usr/src/app
 
-# Copy the current directory contents into the container at /usr/src/app
 COPY . .
 
-# Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Install TensorFlow
-RUN pip install tensorflow
 
-# Make port 5000 available to the world outside this container
 EXPOSE 5000
 
-# Define environment variable
 ENV NAME World
 
-# Run app.py when the container launches
 CMD ["python", "./app.py"]
--- app.py
+++ app.py
@@ -2,38 +2,52 @@ from flask import Flask, request, render_template
 import doctr
 from PIL import Image
 import io
+from doctr.models import ocr_predictor
 from pdf2image import convert_from_bytes
+import numpy as np
 
 app = Flask(__name__)
 
-def perform_ocr_on_image(image):
-    model = doctr.models.predictor('crnn_vgg16_bn', pretrained=True)
-    result = model([image])
-    return result[0]['value']
 
-@app.route('/', methods=['GET', 'POST'])
+def perform_ocr_on_image(image):
+    np_image = np.array(image)
+
+    model = ocr_predictor("db_resnet50", "crnn_vgg16_bn", pretrained=True)
+    document = model([np_image])
+
+    extracted_text = ""
+    for page in document.pages:
+        for block in page.blocks:
+            for line in block.lines:
+                for word in line.words:
+                    extracted_text += word.value + " "
+                extracted_text += "\n"
+            extracted_text += "\n"
+    return extracted_text
+
+
+@app.route("/", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return 'No file part'
-        file = request.files['file']
-        if file.filename == '':
-            return 'No selected file'
+    if request.method == "POST":
+        if "file" not in request.files:
+            return "No file part"
+        file = request.files["file"]
+        if file.filename == "":
+            return "No selected file"
 
         if file:
-            extracted_text = ''
-            if file.filename.endswith('.pdf'):
-                # Convert PDF to images
+            extracted_text = ""
+            if file.filename.endswith(".pdf"):
                 images = convert_from_bytes(file.read())
                 for image in images:
-                    extracted_text += perform_ocr_on_image(image) + '\n'
+                    extracted_text += perform_ocr_on_image(image) + "\n"
             else:
-                # Handle other image types
                 image = Image.open(io.BytesIO(file.read()))
                 extracted_text = perform_ocr_on_image(image)
 
-            return f'Text extracted: {extracted_text}'
-    return render_template('upload.html')
+            return f"Text extracted: {extracted_text}"
+    return render_template("upload.html")
 
-if __name__ == '__main__':
-    app.run(debug=False)
+
+if __name__ == "__main__":
+    app.run(debug=True)
